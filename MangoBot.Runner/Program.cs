67581f1b750b5844f5b24a0dea9583bef2937a79
Marco De Nittis <marco.denittis@gmail.com>
2023-12-04 14:56:30 +0100

    cleanup on program

--- MangoBot.Runner/Program.cs
+++ MangoBot.Runner/Program.cs
@@ -1,25 +1,20 @@
-using Discord;
-using Discord.Commands;
-using Discord.WebSocket;
 using MangoBot.Runner.SK;
-using MangoBot.Runner.Utils;
-using Microsoft.Extensions.Configuration;
 
 namespace MangoBot.Runner;
 
-public class Program
+public static class Program
 {
     public static async Task Main(string[] args)
     {
         var engine = new DiscordEngine(Config.Instance.DiscordToken);
-        //var bot = new KernelBotOne(engine);
-        var bot = new KernelBotTwoWithMemory(engine);
+        
+        var bot = new KernelBotOne(engine);
+        // var bot = new KernelBotTwoWithMemory(engine);
         // var bot = new KernelBotThreeWithPlanner(engine);
 
         await engine.Start();
         await bot.Init();
 
-        // Block this task until the program is closed.
         await Task.Delay(-1);
     }
 }
